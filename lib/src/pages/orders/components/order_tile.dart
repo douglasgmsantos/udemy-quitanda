69de158f70e13c1ecd0e235d8925233b487d7fbf
Douglas Santos <douglas_motasts@outlook.com>
2023-08-14 19:39:29 -0300

    FEAT: adicionando componente de total do pedido e ver QR code

--- lib/src/pages/orders/components/order_tile.dart
+++ lib/src/pages/orders/components/order_tile.dart
@@ -33,6 +33,7 @@ class OrderTile extends StatelessWidget {
             ],
           ),
           childrenPadding: const EdgeInsets.fromLTRB(16, 0, 16, 16),
+          expandedCrossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
             IntrinsicHeight(
               child: Row(
@@ -66,9 +67,32 @@ class OrderTile extends StatelessWidget {
                       status: order.status,
                       isOverdue: order.overdueDateTime.isBefore(DateTime.now()),
                     ),
-                  )
+                  ),
                 ],
               ),
+            ),
+
+            //Total
+            Text.rich(TextSpan(style: const TextStyle(fontSize: 20), children: [
+              const TextSpan(
+                  text: "Total ",
+                  style: TextStyle(fontWeight: FontWeight.bold)),
+              TextSpan(text: utilsServices.priceToCurrency(order.total))
+            ])),
+
+            //Botão de pagamento
+            Visibility(
+              visible: order.status == 'pending_payment',
+              child: ElevatedButton.icon(
+                  style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20))),
+                  onPressed: () {},
+                  icon: Image.asset(
+                    'assets/app_images/pix.png',
+                    height: 18,
+                  ),
+                  label: const Text("Ver QR Code Pix")),
             )
           ],
         ),
